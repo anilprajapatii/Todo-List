8f7d3cfa945d0c678a1c6a8509863b4abbbdf997
Anil prajapati <its.anilprajapatii@gmail.com>
2023-12-07 22:47:50 +0530

    working on reducers

--- src/components/TodoList/TodoLIst.jsx
+++ src/components/TodoList/TodoLIst.jsx
@@ -3,6 +3,31 @@ import TodoContext from "../../context/TodoContext";
 import { useContext } from "react";
 function TodoList() {
   const {list, setList} = useContext(TodoContext);
+
+  function onFinished(todo,isFinished){
+    const updatedList = list.map(t => {
+      if (t.id == todo.id) {
+        todo.finished = isFinished;
+      }
+      return t;
+    });
+    setList(updatedList);
+  }
+  function onDelete(todo){
+    const updatedList = list.filter(t => t.id != todo.id);
+    setList(updatedList);
+  }
+
+  function onEdit(todo,todoText){
+    const updatedList = list.map(t => {
+      if (t.id == todo.id) {
+        todo.todoData = todoText;
+      }
+      return t;
+    });
+    setList(updatedList);
+  }
+
   return (
     <div>
       {list.length > 0 &&
@@ -11,28 +36,9 @@ function TodoList() {
             id={todo.id}
             isFinished={todo.finished}
             todoData={todo.todoData}
-            changeFinished={(isFinished) => {
-              const updatedList = list.map(t => {
-                if (t.id == todo.id) {
-                  todo.finished = isFinished;
-                }
-                return t;
-              });
-              setList(updatedList);
-            }}
-            onDelete={() => {
-              const updatedList = list.filter(t => t.id != todo.id);
-              setList(updatedList);
-            }}
-            onEdit={(todoText) => {
-              const updatedList = list.map(t => {
-                if (t.id == todo.id) {
-                  todo.todoData = todoText;
-                }
-                return t;
-              });
-              setList(updatedList);
-            }}  
+            changeFinished={(isFinished) => onFinished(todo,isFinished)}
+            onDelete={() => onDelete(todo)}
+            onEdit={(todoText) => onEdit(todo,todoText)}  
           />
         )}
     </div>
